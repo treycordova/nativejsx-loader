5e697e3c3e844073ae9693c98d0acd948360740d
odzb <odzb@users.noreply.github.com>
2017-04-03 17:57:14 +0300

    Don't include global prorotypes if they are inlined
    Fix for https://github.com/treycordova/nativejsx-loader/issues/5

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ module.exports = function(source) {
   this.cacheable && this.cacheable(true);
 
   var query = loaderUtils.parseQuery(this.query);
-  var tree = [
-    "require('nativejsx/dist/nativejsx-prototypes.js');",
-    nativejsx.transpile(source, query)
-  ];
+  var tree = [];
+
+  if (query && (query.prototypes !== 'inline')) {
+    tree.push("require('nativejsx/dist/nativejsx-prototypes.js');");
+  }
+  tree.push(nativejsx.transpile(source, query));
 
   return tree.join('\n');
 };
